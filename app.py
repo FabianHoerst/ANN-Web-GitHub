05ede30a0bdc774598c0a278b640f08e12e20b82
FabianHoerst <67600643+FabianHoerst@users.noreply.github.com>
2021-01-17 18:39:10 +0100

    Change comments in app.py

--- app.py
+++ app.py
@@ -2,12 +2,14 @@
 Starting the Appserver and loading the Stylesheet
 '''
 
+# import dash modules
 import dash
 import dash_bootstrap_components as dbc
 
 # css theme, please see documentation here: https://dash-bootstrap-components.opensource.faculty.ai/docs/themes/
 external_css = [dbc.themes.DARKLY]
 
+# start server
 app = dash.Dash(__name__, external_stylesheets=external_css)
 app.title = 'ANN'
 server = app.server
